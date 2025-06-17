aaff1d09dfbef59b43de70b6618904ef1e9a4c01
Kiran Thakur <kiranthakur@1407@gmail.com>
2025-06-17 15:01:42 +0530

    update code

--- apps.js
+++ apps.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express();
 const cors = require("cors"); 
 const UserModel = require("./models/user")
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 
 
 app.use(cors()); 
--- models/user.js
+++ models/user.js
@@ -1,14 +1,45 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://127.0.0.1:27017/thakurconeect');
+
+// const userSchema = mongoose.Schema({
+//     username: String,
+//     email: String,
+//   password: String,
+//   name:String,
+//   message:String
+  
+// });
+
+// module.exports = mongoose.model("user", userSchema);
+
+
+
+
+
+
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/thakurconeect');
+// Load .env file
+require('dotenv').config();
 
-const userSchema = mongoose.Schema({
-    username: String,
-    email: String,
-  password: String,
-  name:String,
-  message:String
-  
+// Connect using Atlas URI from .env
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("MongoDB Atlas connected successfully");
+}).catch((err) => {
+  console.error("MongoDB Atlas connection failed:", err);
 });
 
-module.exports = mongoose.model("user", userSchema);
+const userSchema = mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  name: String,
+  message: String
+});
+
+module.exports = mongoose.model("user", userSchema);
